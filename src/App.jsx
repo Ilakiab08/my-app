d9f1b9f93411ca687775df6f8042e4b0145c3dfb
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-01-16 17:35:12 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import "./App.css";
+
+import NavBar from "./components/NavBar/NavBar";
+
+import Footer from "./components/Footer/Footer";
+
+function App() {
+ 
+  return (
+    <div classname ='App'>
+      <header classname='header'>
+        <h1>Welcome to our website!</h1>
+        <p>This is a simple web application built using React.</p>
+        </header>
+        <main>
+          <NavBar />
+          <section className="content">
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing el
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              do eiusmod tempor incididunt ut labore et dolore magna aliqu
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              quia non numquam ei deseruit. Amantium enim neque qui
+              est in his tempus voluptatibus ut cumque nulla facere possim
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed </p>
+              </section>
+              </main>
+              <Footer/>
+              </div>
+              );
+              }
+              export default App;
+
+
+
+        
+
+
+            
+     
